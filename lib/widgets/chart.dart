3645be523bc1e2f83eafe46b06e11f272532a20c
03654916163 <thales.t@gmail.com>
2019-10-07 14:03:02 -0300

    ...

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -38,7 +38,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Card(
+    return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Padding(
